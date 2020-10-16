b1f1f6131f119adb3c61a46db2fc92fbdca00310
FabioSebs <sebsphotosofficial@gmail.com>
2020-10-16 00:25:41 -0400

    improved CharCount

--- CharCount/charcounter.py
+++ CharCount/charcounter.py
@@ -3,9 +3,8 @@ def char_freq(word:str):
     #problem is that there are duplicates
     char_count = [str(word.count(i)) for i in word]
     char_list = [i for i in word]
-    print(char_list)
-    print(char_count)
-    return dict(zip(char_count, char_list))
+
+    return dict(zip(char_list, char_count))
     
     
     
--- CharCount/main.py
+++ CharCount/main.py
@@ -1,4 +1,4 @@
 import charcounter as cc
 
 if __name__ == "__main__":
-    cc.char_freq("hippopotamus")
+   print(cc.char_freq("hippopotamus"))
